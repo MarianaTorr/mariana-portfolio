44f90facda3ccfe9cad33426d22a95d7b8f86e0c
MarianaTorr <96776065+MarianaTorr@users.noreply.github.com>
2025-12-08 13:57:44 +0100

    Create navbar-loader.js

---
+++ navbar-loader.js
@@ -0,0 +1,9 @@
+// Loads the navbar.html file and injects it into the placeholder div
+fetch("navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
+  })
+  .catch(error => {
+    console.error("Error loading navbar:", error);
+  });
