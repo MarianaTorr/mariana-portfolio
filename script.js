40c1261679751c851a00da5cd0abf24f6e39906b
MarianaTorr <96776065+MarianaTorr@users.noreply.github.com>
2025-12-26 17:32:19 +0000

    z

---
+++ script.js
@@ -0,0 +1,24 @@
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlay-img");
+const overlayVideo = document.getElementById("overlay-video");
+const close = document.getElementById("close");
+
+document.querySelectorAll(".project-image img, .project-image video").forEach(item => {
+  item.addEventListener("click", () => {
+    overlay.style.display = "flex";
+    if(item.tagName === "IMG") {
+      overlayImg.src = item.src;
+      overlayImg.style.display = "block";
+      overlayVideo.style.display = "none";
+    } else {
+      overlayVideo.src = item.src;
+      overlayVideo.style.display = "block";
+      overlayImg.style.display = "none";
+    }
+  });
+});
+
+close.addEventListener("click", () => {
+  overlay.style.display = "none";
+  overlayVideo.pause();
+});
